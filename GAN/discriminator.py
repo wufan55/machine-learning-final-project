76a71365c2e73dddbd2398938c24ccf9a35e8408
Fan <fan@FandeMacBook-Pro.local>
2019-05-30 10:16:16 +0800

    update

--- GAN/discriminator.py
+++ GAN/discriminator.py
@@ -36,7 +36,17 @@ def pool_layer(name, input, kernel_size):
 
 
 # 全连接层
-# def full_connect_layer(name, input):
+# 使用relu激活函数
+def full_connect_layer(name, input, nodes):
+    input_shape = input.get_shape().as_list()
+
+    with tf.variable_scope(name):
+        weight = tf.get_variable('weight', shape=[input_shape[1], nodes],
+                                 initializer=tf.truncated_normal_initializer(mean=0, stddev=0.1))
+        bias = tf.get_variable('bias', shape=[nodes], initializer=tf.zeros_initializer())
+
+    fc = tf.matmul(input, weight)
+    return tf.nn.relu(tf.nn.bias_add(fc, bias))
 
 
 # 定义网络结构
@@ -54,6 +64,20 @@ conv_layer_2_output = conv_layer(name='conv_layer_2', input=pool_layer_1_output,
 
 # shape (4, 4, 4)
 pool_layer_2_output = pool_layer(name='pool_layer_2', input=conv_layer_2_output, kernel_size=2)
+
+# 进入全连接前的平铺操作
+# shape (1, 64)
+fc_input = tf.reshape(pool_layer_2_output, [BATCH_SIZE, 64])
+
+# shape (1, 10)
+fc_layer_1_output = full_connect_layer(name='fc_layer_1', input=fc_input, nodes=10)
+
+# shape (1, 1)
+fc_layer_2_output = full_connect_layer(name='fc_layer_2', input=fc_layer_1_output, nodes=1)
+
+# shape (1, 1)
+y_output = fc_layer_2_output
+
 
 # 变量初始化
 init = tf.global_variables_initializer()
